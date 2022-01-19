3af8afc56eb4d5b55fd5b2edb33cf13a3ab6c648
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-01-19 14:59:38 +0530

    add document object model

--- documents/models.py
+++ documents/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Document(models.Model):
+    DOCUMENT_STATUS_CHOICES = (
+        ('open', 'Open'),
+        ('closed', 'Closed'),
+    )
+    name = models.CharField(max_length=150, null=True, blank=True)
+    doc_file = models.FileField(upload_to='uploads/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='documents')
+    shared_with = models.ManyToManyField(User, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    modified_on = models.DateTimeField(auto_now=True, null=True, blank=True)
+    status = models.CharField(max_length=30, choices=DOCUMENT_STATUS_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+
