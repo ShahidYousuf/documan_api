e007ae3d8afe1b1ced1f25f4ec46c2f687b52863
Shahid Yousuf <shahidyousuf77@gmail.com>
2022-01-21 12:54:22 +0530

    - add download action and log the action in the download resource for demo

--- documents/views.py
+++ documents/views.py
@@ -1,7 +1,12 @@
+import datetime
+
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from django.conf import settings
+from rest_framework import viewsets, status
 from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Document
 from .serializers import DocumentSerializer
 from .permissions import ConditionsPermission
@@ -14,5 +19,25 @@ class DocumentViewSet(viewsets.ModelViewSet):
 
     def retrieve(self, request, *args, **kwargs):
         return super(DocumentViewSet, self).retrieve(request, *args, **kwargs)
+
+    @action(detail=True, methods=['get', 'retrieve'])
+    def download(self, request, pk=None):
+        document = self.get_object()
+        url = request.build_absolute_uri(document.doc_file.url)
+        owner_editing = document.owner in document.current_editors.all()
+        is_collaborator = request.user in document.shared_with.all() and request.user != document.owner
+        if is_collaborator and owner_editing:
+            return Response({'status': 'Cannot download, owner is currently editing the document'}, status=status.HTTP_403_FORBIDDEN)
+        role = "OWNER" if document.owner == request.user else "COLLABORATOR"
+        date = datetime.datetime.now()
+        path = str(settings.BASE_DIR) + document.doc_file.url
+        log_text = f"\n{date} --- DOWNLOAD by {role} -- {request.user.username}\n"
+        with open(path, 'a') as f:
+            f.write(log_text)
+        return Response({
+            'download': url,
+            'status': 'Click the link above to download the document.'
+        })
+
 
 
